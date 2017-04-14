78ca94c504bbdd3ce754a7b3a97bee36d88ef30e
Sourav Kumar <souravkumarc4@gmail.com>
2017-04-15 02:13:12 +0530

    added ver3

--- js/controllers.js
+++ js/controllers.js
@@ -1,18 +1,45 @@
 app.controller("indexCtrl", function ($scope) {
-
+var liarray = document.getElementsByClassName("headmenu");
+	liarray[0].className += " current";
+	liarray[1].className = liarray[1].className.replace( /(?:^|\s)current(?!\S)/g , '' );
+	liarray[2].className = liarray[1].className.replace( /(?:^|\s)current(?!\S)/g , '' );
+	liarray[3].className = liarray[1].className.replace( /(?:^|\s)current(?!\S)/g , '' );
+	var x = document.getElementById('banner');
+	x.style.display = 'block';
+	document.getElementById("header").style.backgroundColor = "transparent";
 });
 
 app.controller("blogCtrl", function ($scope) {
-/*var x = document.getElementById('banner');
-x.style.display = 'none';*/
+var liarray = document.getElementsByClassName("headmenu");
+	liarray[0].className = liarray[0].className.replace( /(?:^|\s)current(?!\S)/g , '' );
+	liarray[1].className += " current";
+	liarray[2].className = liarray[1].className.replace( /(?:^|\s)current(?!\S)/g , '' );
+	liarray[3].className = liarray[1].className.replace( /(?:^|\s)current(?!\S)/g , '' );
+	var x = document.getElementById('banner');
+	x.style.display = 'none';
+	document.getElementById("header").style.backgroundColor = "#3f7a8d";
 });
 
 app.controller("contCtrl", function ($scope) {
-
+var liarray = document.getElementsByClassName("headmenu");
+	liarray[0].className = liarray[0].className.replace( /(?:^|\s)current(?!\S)/g , '' );
+	liarray[1].className = liarray[1].className.replace( /(?:^|\s)current(?!\S)/g , '' );
+	liarray[2].className += " current";
+	liarray[3].className = liarray[1].className.replace( /(?:^|\s)current(?!\S)/g , '' );
+	var x = document.getElementById('banner');
+	x.style.display = 'none';
+	document.getElementById("header").style.backgroundColor = "#3f7a8d";
 });
 
 app.controller("helpCtrl", function ($scope) {
-
+var liarray = document.getElementsByClassName("headmenu");
+	liarray[0].className = liarray[0].className.replace( /(?:^|\s)current(?!\S)/g , '' );
+	liarray[1].className = liarray[1].className.replace( /(?:^|\s)current(?!\S)/g , '' );
+	liarray[2].className = liarray[1].className.replace( /(?:^|\s)current(?!\S)/g , '' );
+	liarray[3].className += " current";
+	var x = document.getElementById('banner');
+	x.style.display = 'none';
+	document.getElementById("header").style.backgroundColor = "#3f7a8d";
 });
 
 
